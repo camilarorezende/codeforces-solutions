46843db5cf8e6c89974ffe58d6be0be07d71d73d
camilarorezende <camilarorezende@gmail.com>
2026-08-20 09:24:38 -0300

    fix: solution Hit the lottery

--- ratings/800/hitTheLottery.py
+++ ratings/800/hitTheLottery.py
@@ -7,13 +7,19 @@ value = int(input())
 
 notas = [100, 20, 10, 5, 1]
 count = 0
-id = 0
 
-while (id < 5 and value > 0):
-    if value - notas[id] >= 0:
-        value -= notas[id]
-        count+=1
-    else:
-        id+=1
+## this solution works but it has time limit exceeded problem
+## id = 0
+
+## while (id < 5 and value > 0):
+##    if value - notas[id] >= 0:
+##        value -= notas[id]
+##        count+=1
+##    else:
+##        id+=1
+
+for nota in notas:
+    count += value // nota
+    value %= nota
 
 print(count)
